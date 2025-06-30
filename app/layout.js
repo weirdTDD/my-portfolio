bb139f039a3798847c73862c3f27061e3501eda9
weirdTDD <157230268+weirdTDD@users.noreply.github.com>
2025-06-30 20:58:01 +0100

    Initial commit

--- app/layout.js
+++ app/layout.js
@@ -16,10 +16,10 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className='scroll-smooth dark' >
+    <html lang="en" className='scroll-smooth ' >
       <body
         className={`${outfit.className
-        } ${ovo.className} antialiased leading-8 overflow-x-hidden dark:bg-darkTheme dark:text-white`}
+        } ${ovo.className} antialiased leading-8 overflow-x-hidden d`}
       >
         {children}
       </body>
